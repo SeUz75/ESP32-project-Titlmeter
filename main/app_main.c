8a10bdba0799f88b912ec339a73a326931743971
SeUz75 <seuz752@gmail.com>
2026-06-24 16:14:43 +0300

    ESP32 Project: 	- I2C configuration of gmeter and also pwm for controlling motor. As we move the gmeter on X axis the C program converts raw x data into 		angle degrees. Really interesting way of convertion. Moving forward see where we get :D

--- main/app_main.c
+++ main/app_main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <inttypes.h> // Required for PRIu8
+#include <math.h>
 #include "driver/i2c_master.h"
 #include "soc/clk_tree_defs.h"
 #include "freertos/FreeRTOS.h"
@@ -30,7 +31,7 @@ static const char *TAG = "main";
 
 void configure_i2c_accelerometer(i2c_master_bus_config_t* master_conf,
         i2c_master_bus_handle_t* master_handle,
-        i2c_device_config_t* slave_conf,
+        i2c_device_config_t*     slave_conf,
         i2c_master_dev_handle_t* master_slave_handle);
 
 void find_slave_address(i2c_master_bus_handle_t* master_handle_p);
@@ -38,7 +39,16 @@ void find_slave_address(i2c_master_bus_handle_t* master_handle_p);
 void recycle_resources(i2c_master_bus_handle_t master_bus_handle,
         i2c_master_dev_handle_t master_dev_handle);
 
-void configure_servo();
+void configure_mcpwm_servo(mcpwm_timer_config_t* timer_struct,
+        mcpwm_timer_handle_t*      timer_handle,
+        mcpwm_operator_config_t*   operator_struct,
+        mcpwm_oper_handle_t*       operator_handle,
+        mcpwm_comparator_config_t* comparator_struct,
+        mcpwm_cmpr_handle_t*       comparator_handle,
+        mcpwm_generator_config_t*  generator_struct,
+        mcpwm_gen_handle_t*  generator_handle);
+
+static uint32_t example_angle_to_compare(int angle);
 
 void app_main(void) {
     // Gmeter configuration and connection master bus to device
@@ -62,6 +72,11 @@ void app_main(void) {
     mcpwm_generator_config_t generator_struct = {0};
     mcpwm_gen_handle_t generator_handle = NULL;
 
+    configure_mcpwm_servo(&timer_struct, &timer_handle,
+                          &operator_struct, &operator_handle,
+                          &comparator_struct, &comparator_handle,
+                          &generator_struct, &generator_handle);
+
 
     // Master specifying in slave which memory to read ! Check your data sheets !
 
@@ -73,6 +88,10 @@ void app_main(void) {
     const uint8_t write_buff[2] = {POWER_CTL, 0x08};
     i2c_master_transmit(master_slave_handle, write_buff, sizeof(write_buff), -1);
     int16_t x_axis, y_axis, z_axis;
+    float x_float, y_float, z_float, angle_rad;
+    int target_angle;
+
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator_handle, example_angle_to_compare(0)));
 
     while(1) {
         esp_err_t err_status =  i2c_master_transmit_receive(master_slave_handle,
@@ -82,7 +101,18 @@ void app_main(void) {
                 x_axis = (int16_t)((receive_buffer[1] << 8) | receive_buffer[0]);
                 y_axis = (int16_t)((receive_buffer[3] << 8) | receive_buffer[2]);
                 z_axis = (int16_t)((receive_buffer[5] << 8) | receive_buffer[4]);
-                printf("x -> %d\ny-> %d\nz-> %d\n", x_axis,y_axis,z_axis);
+
+                x_float = (float)(x_axis);
+                y_float = (float)(y_axis);
+                z_float = (float)(z_axis);
+                angle_rad = atan2f(x_float, sqrtf((y_float * y_float) + (z_float * z_float)));
+                target_angle = (int)(angle_rad * 180.0f / M_PI); // M_PI is defined in math.h
+
+                if (target_angle < SERVO_MIN_DEGREE) target_angle = SERVO_MIN_DEGREE;
+                if (target_angle > SERVO_MAX_DEGREE) target_angle = SERVO_MAX_DEGREE;
+
+                ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator_handle, example_angle_to_compare(target_angle)));
+                printf("Raw X: %d | Calculated Angle: %d°\n", x_axis, target_angle);
                 vTaskDelay(pdMS_TO_TICKS(100));
             }
             else {
@@ -150,73 +180,62 @@ void recycle_resources(i2c_master_bus_handle_t master_bus_handle,
 
 
 // Servo motors
-
+    // Helper function to convert a desired servo angle in degrees
+    // into a raw compare value (pulse width in microseconds).
 static inline uint32_t example_angle_to_compare(int angle)
 {
     return (angle - SERVO_MIN_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (SERVO_MAX_DEGREE - SERVO_MIN_DEGREE) + SERVO_MIN_PULSEWIDTH_US;
 }
 
-void configure_servo() {
-    mcpwm_timer_handle_t servo_handle = NULL;
+void configure_mcpwm_servo(mcpwm_timer_config_t* timer_struct,
+        mcpwm_timer_handle_t*      timer_handle,
+        mcpwm_operator_config_t*   operator_struct,
+        mcpwm_oper_handle_t*       operator_handle,
+        mcpwm_comparator_config_t* comparator_struct,
+        mcpwm_cmpr_handle_t*       comparator_handle,
+        mcpwm_generator_config_t*  generator_struct,
+        mcpwm_gen_handle_t*  generator_handle) {
 
-    const mcpwm_timer_config_t timer_cfg = {
+    // Timer configuration
+    (*timer_struct) = (mcpwm_timer_config_t){
         .group_id = 0,                                // Use MCPWM Group 0
         .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,       // Use the default system clock source
         .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,// Set clock to 1 tick per microsecond
         .period_ticks = SERVO_TIMEBASE_PERIOD,        // Count up to 20,000 ticks (50Hz)
         .count_mode = MCPWM_TIMER_COUNT_MODE_UP,      // Count upwards
     };
-    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_cfg, &servo_handle));
+    ESP_ERROR_CHECK(mcpwm_new_timer(timer_struct, timer_handle));
 
-
-    // Operator
-    mcpwm_oper_handle_t oper = NULL;
-    mcpwm_operator_config_t conf = {
+    // Operator configuration
+    (*operator_struct) = (mcpwm_operator_config_t) {
         .group_id = 0, // operator must be in the same group to the timer
     };
 
-    ESP_ERROR_CHECK(mcpwm_new_operator(&conf, &oper));
-    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, servo_handle));
+    ESP_ERROR_CHECK(mcpwm_new_operator(operator_struct, operator_handle));
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer((*operator_handle), (*timer_handle)));
 
-    // Comparator
-    mcpwm_comparator_config_t comparator_config = {
+    // Comparator configuration
+    (*comparator_struct) = (mcpwm_comparator_config_t){
         .flags.update_cmp_on_tez = true,
     };
-    mcpwm_cmpr_handle_t comparator;
 
-    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &comparator_config, &comparator));
+    ESP_ERROR_CHECK(mcpwm_new_comparator((*operator_handle), comparator_struct, comparator_handle));
 
-
-    // Generator
-
-    mcpwm_gen_handle_t generator = NULL;
-    mcpwm_generator_config_t generator_config = {
+    // Generator configuration
+    (*generator_struct) = (mcpwm_generator_config_t){
         .gen_gpio_num = AZIMUTH_MOTOR,
     };
-    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &generator_config, &generator));
+    ESP_ERROR_CHECK(mcpwm_new_generator((*operator_handle), generator_struct, generator_handle));
 
 
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(0)));
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value((*comparator_handle), example_angle_to_compare(0)));
 
-    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(generator,
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event((*generator_handle),
                                                               MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
-    // go low on compare threshold
-    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generator,
-                                                                MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
+    // // go low on compare threshold
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event((*generator_handle),
+                                                                MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, (*comparator_handle), MCPWM_GEN_ACTION_LOW)));
 
-    ESP_ERROR_CHECK(mcpwm_timer_enable(servo_handle));
-    ESP_ERROR_CHECK(mcpwm_timer_start_stop(servo_handle, MCPWM_TIMER_START_NO_STOP));
-
-
-    int angle = 0;
-    int step = 2;
-    while (1) {
-        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, example_angle_to_compare(angle)));
-        //Add delay, since it takes time for servo to rotate, usually 200ms/60degree rotation under 5V power supply
-        vTaskDelay(pdMS_TO_TICKS(500));
-        if ((angle + step) > 60 || (angle + step) < -60) {
-            step *= -1;
-        }
-        angle += step;
-    }
+    ESP_ERROR_CHECK(mcpwm_timer_enable((*timer_handle)));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop((*timer_handle), MCPWM_TIMER_START_NO_STOP));
 }
